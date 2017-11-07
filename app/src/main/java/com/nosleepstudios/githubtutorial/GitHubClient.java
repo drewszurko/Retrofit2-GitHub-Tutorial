6a524ee61dd2b1a8c8a70143bb26a9dc8cb58e68
Drew Szurko <andrewszurko@gmail.com>
2017-11-06 22:09:34 -0600

    Cleaned code spacing

--- app/src/main/java/com/nosleepstudios/githubtutorial/GitHubClient.java
+++ app/src/main/java/com/nosleepstudios/githubtutorial/GitHubClient.java
@@ -7,7 +7,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface GitHubClient {
-
     @GET("/users/{user}/repos")
     Call<List<GitHubRepo>> reposForUser(@Path("user") String user);
 }
