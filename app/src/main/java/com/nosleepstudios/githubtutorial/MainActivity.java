c9ac6279d9a89be715dc590b5906c2171e122cb8
Drew Szurko <andrewszurko@gmail.com>
2017-11-06 22:11:21 -0600

    Cleaned code spacing

--- app/src/main/java/com/nosleepstudios/githubtutorial/MainActivity.java
+++ app/src/main/java/com/nosleepstudios/githubtutorial/MainActivity.java
@@ -13,12 +13,10 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
     private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
